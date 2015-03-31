fbe70b22cac6ae393d1b7ebed1ebcf72133abac4
Jose+Rhoen <Jose+Rhoen@appacademy.io>
2015-03-31 12:28:04 -0400

    Write piece classes skeleton

--- piece.rb
+++ piece.rb
@@ -1,9 +1,10 @@
 class Piece
-  attr_reader :color
+  attr_reader :color, :position
 
-  def initialize(color, board)
+  def initialize(color, board, position)
     @color = color
     @board = board
+    @postion = position
   end
 
   def moves
@@ -12,4 +13,41 @@ class Piece
 
   protected
   attr_reader :board
+  attr_writer :position
+end
+
+class SlidingPiece < Piece
+
+  def move_dirs
+    raise NotImplementedError
+  end
+
+end
+
+class Bishop < SlidingPiece
+
+end
+
+class Rook < SlidingPiece
+
+end
+
+class Queen < SlidingPiece
+
+end
+
+class SteppingPiece < Piece
+
+end
+
+class Knight < SteppingPiece
+
+end
+
+class King < SteppingPiece
+
+end
+
+class Pawn < Piece
+
 end
