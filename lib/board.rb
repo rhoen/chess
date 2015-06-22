86f3a9563955aa9fc74033ef1f27028134737c82
Rhoen Pruesse-Adams <rhoen.pa@gmail.com>
2015-06-22 14:24:12 -0400

    refactor board#move

--- lib/board.rb
+++ lib/board.rb
@@ -53,8 +53,8 @@ class Board
   # refactor move and move! like all_available_moves
   def move(start, end_pos)
     piece = self[start]
-    available_moves = self[start].valid_moves
-    raise MoveNotAvailableError if !available_moves.include?(end_pos)
+    available_moves = piece.valid_moves
+    raise MoveNotAvailableError unless available_moves.include?(end_pos)
     piece.move_to(end_pos)
   rescue MoveNotAvailableError => e
     puts "Piece cannot move to that location!"
